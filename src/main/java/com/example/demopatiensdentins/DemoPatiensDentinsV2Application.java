957bdbec667780b0e72846a416cd561f352a8d60
Ander <andersonfrias001@gmail.com>
2022-10-14 16:58:54 -0400

    Version 0

---
+++ src/main/java/com/example/demopatiensdentins/DemoPatiensDentinsV2Application.java
@@ -0,0 +1,13 @@
+package com.example.demopatiensdentins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPatiensDentinsV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPatiensDentinsV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demopatiensdentins/DemoPatiensDentinsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demopatiensdentins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPatiensDentinsV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
